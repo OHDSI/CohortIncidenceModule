6195491447ba49765d6b68dfc23dafc102550289
Chris Knoll <cknoll@ohdsi.org>
2023-06-22 10:05:45 -0400

    Update SettingsFunctions.R

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -7,7 +7,7 @@ createCohortIncidenceModuleSpecifications <- function(irDesign = NULL) {
   }
 
   specifications <- list(module = "CohortIncidenceModule",
-                         version = "0.0.7",
+                         version = "0.0.8",
                          remoteRepo = "github.com",
                          remoteUsername = "ohdsi",
                          settings = analysis)
