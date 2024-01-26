c11c2a4db2ad00afa48ed5b0b487dbe006fb6b14
Anthony Sena <anthonysena@users.noreply.github.com>
2024-01-26 12:09:03 -0500

    Use renv project profiles (#12)
    * Sync hades-wide lock file
    
    * Update GHA
    
    * Apply readr workaround
    
    * Release prep

--- extras/debug.R
+++ extras/debug.R
@@ -1,6 +1,6 @@
 connectionDetails <- Eunomia::getEunomiaConnectionDetails()
 Eunomia::createCohorts(connectionDetails)
-endDateFixSql <-"update main.cohort set cohort_end_date = dateadd(d,1, cohort_start_date) where cohort_end_date is null"
+endDateFixSql <- "update main.cohort set cohort_end_date = dateadd(d,1, cohort_start_date) where cohort_end_date is null"
 conn <- DatabaseConnector::connect(connectionDetails = connectionDetails)
 DatabaseConnector::renderTranslateExecuteSql(connection = conn, sql = endDateFixSql)
 DatabaseConnector::dbDisconnect(conn)
@@ -11,17 +11,18 @@ resultsfolder <- tempfile("results")
 dir.create(resultsfolder)
 jobContext <- readRDS("tests/testJobContext.rds")
 jobContext$moduleExecutionSettings$workSubFolder <- workFolder
-jobContext$moduleExecutionSettings$resultsSubFolder  <- resultsfolder
+jobContext$moduleExecutionSettings$resultsSubFolder <- resultsfolder
 jobContext$moduleExecutionSettings$connectionDetails <- connectionDetails
 
-buildOptions <- CohortIncidence::buildOptions(cohortTable = paste0(jobContext$moduleExecutionSettings$workDatabaseSchema, '.', jobContext$moduleExecutionSettings$cohortTableNames$cohortTable),
-                                              cdmDatabaseSchema = jobContext$moduleExecutionSettings$cdmDatabaseSchema,
-                                              sourceName = as.character(jobContext$moduleExecutionSettings$databaseId))
+buildOptions <- CohortIncidence::buildOptions(
+  cohortTable = paste0(jobContext$moduleExecutionSettings$workDatabaseSchema, ".", jobContext$moduleExecutionSettings$cohortTableNames$cohortTable),
+  cdmDatabaseSchema = jobContext$moduleExecutionSettings$cdmDatabaseSchema,
+  sourceName = as.character(jobContext$moduleExecutionSettings$databaseId)
+)
 
-irDesign <- as.character(CohortIncidence::IncidenceDesign$new(jobContext$settings$irDesign)$asJSON());
-
-
-analysisSql <- CohortIncidence::buildQuery(incidenceDesign =  irDesign,
-                                           buildOptions = buildOptions);
-
+irDesign <- as.character(CohortIncidence::IncidenceDesign$new(jobContext$settings$irDesign)$asJSON())
+analysisSql <- CohortIncidence::buildQuery(
+  incidenceDesign = irDesign,
+  buildOptions = buildOptions
+)
 analysisSql <- SqlRender::translate(analysisSql, targetDialect = "sqlite")
